3a61e5bb4853395d7948600e78eff2a24d9a95ab
NamohS <112482434+NamohS@users.noreply.github.com>
2023-10-10 14:40:49 +0530

    Create base_cnvrt_bitset.cpp

---
+++ base_cnvrt_bitset.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+main()
+{
+    int var1=23;
+    cout<<var1<<endl; 
+    cout<<showbase<<hex<<var1<<endl;
+    cout<<bitset<100>(var1);
+
+}
